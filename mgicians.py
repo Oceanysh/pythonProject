315410db655b5715c200be985f22e858ba00df0c
Neri <ysh9854@163.com>
2023-12-22 14:47:53 +0800

    4.3练习完结

--- mgicians.py
+++ mgicians.py
@@ -28,3 +28,49 @@ print(numbers)
 # 打印1～10偶数
 even_numbers = list(range(2,11,2))
 print(even_numbers)
+
+# 创建前10个整数的平方
+squares = []
+for value in range(1, 11):
+    square = value**2
+    squares.append(square)
+
+    print(squares)
+
+# 简洁写法
+two_squares = []
+for value in range(1, 11):
+    two_squares.append(value**2)
+
+    print(two_squares)
+
+# 列表解析
+three_squares = [value**2 for value in range(1,11)]
+print(three_squares)
+
+# 4-3 到 4-9
+# “4-3 数到20：使用一个for循环打印数字1~20（含）。”
+for value in range (1,21):
+    print(value)
+# “4-4 一百万：创建一个列表，其中包含数字1~1 000 000，
+# 再使用一个for循环将这些数字打印出来（如果输出的时间太长，按Ctrl + C停止输出，或关闭输出窗口）。”
+
+million_squares = [value for value in range (1,1000001)]
+print(million_squares)
+
+# 4-5 算总和
+print(sum(million_squares))
+
+# 4-6 找奇数
+for value in range(1,20,2):
+    print(value)
+
+# 4-7 3的倍数
+for value in range(3,31,3):
+    print(value)
+
+# 4-8 立方 4-9
+lifang_squares = [value**3 for value in range(1,11)]
+print(lifang_squares)
+
+
